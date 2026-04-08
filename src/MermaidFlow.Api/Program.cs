72273d1afa9fbeaf4abaf13a069e171fdec9579c
Tareq-Bilal <tareqbilal44@gmail.com>
2026-04-08 04:34:57 +0300

    adding serialog

--- src/MermaidFlow.Api/Program.cs
+++ src/MermaidFlow.Api/Program.cs
@@ -2,9 +2,30 @@ using MermaidFlow.Api.Middleware;
 using MermaidFlow.Application;
 using MermaidFlow.Infrastructure;
 using Scalar.AspNetCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting MermaidFlow API...");
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Host.UseSerilog((context, services, config) =>
+    {
+        config
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .WriteTo.Console()
+            .WriteTo.File(
+                path: "logs/mermaidflow-.log",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7);
+    });
+
     builder.Services.AddControllers();
     builder.Services.AddOpenApi();
 
@@ -22,11 +43,21 @@ var app = builder.Build();
     }
 
     app.UseMiddleware<ExceptionHandlingMiddleware>();
+    app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
     app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
 
     app.Run();
+}
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
